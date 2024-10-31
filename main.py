58bfa16cab6ad66857b8e7a6973f8360f1a23320
noahpaquin <noahpaquin7@gmail.com>
2024-10-31 15:39:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-# Put your function here
+def timestable(nums):
+    table = [[1,2,3,4,5],[2,4,6,8,10],[3,6,9,12,15],[5,10,15,20,25]]
+    num = nums
+    return table[0:num]
 
 
-# testing
 nums = int(input())
-print(timesTable(nums))
+print(timestable(nums))
